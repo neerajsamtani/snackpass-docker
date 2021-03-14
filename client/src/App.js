e9264c8baffd4a9c5592cfeb28001093b1c7d372
Neeraj Samtani <neerajjsamtani@gmail.com>
2021-03-14 15:46:55 -0700

    Updated App.js to functional React

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,29 @@
 // client/src/App.js
-import React, { Component } from "react";
+import React, {useState, useEffect} from 'react'
 import axios from "axios";
 
-class App extends Component {
-  state = {
-    welcome: "...."
-  };
+const App = (props) => {
 
-  componentDidMount = async () => {
-    try {
-      const res = await axios.get("/welcome");
-      console.log(res);
-      this.setState({ welcome: res.data });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  render() {
-    return (
-      <div className="App">
-        <h1>"Hello server!" says the client. How are you?</h1>
-        <h1>"{this.state.welcome}" says the server</h1>
-      </div>
-    );
+  const [message, setMessage] = useState("none")
+
+  const hook = () => {
+      console.log('effect')
+      axios
+          .get('/welcome')
+          .then(response => {
+              console.log('promise fulfilled')
+              setMessage(response.data)
+          })
   }
+
+  useEffect(hook, [])
+
+  return (
+    <div className="App">
+        <h1>"Hello server!" says the client. How are you?</h1>
+        <h1>"{message}" says the server</h1>
+      </div>
+  )
 }
 
 export default App;
