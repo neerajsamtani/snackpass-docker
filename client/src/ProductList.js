f730adae2e43109f5c9dfc5bfbcd534deadaa7bc
Neeraj Samtani <neerajjsamtani@gmail.com>
2021-03-15 05:39:27 -0700

    Refactor into multiple components - Product and ProductList

--- client/src/ProductList.js
+++ client/src/ProductList.js
@@ -1,53 +1,14 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
 import Product from './Product'
 
-const ProductList = (props) => {
-
-    const [products, setProducts] = useState([{
-        "Order ID": 25583,
-        "Restaurant": "Hogwarts",
-        "Item Name": "Chicken Tikka Masala",
-        "Quantity": 1,
-        "Product Price": 8.95,
-        "Total products": 12
-      },
-      {
-        "Order ID": 25582,
-        "Restaurant": "Hogwarts",
-        "Item Name": "Tandoori King Prawn Masala",
-        "Quantity": 1,
-        "Product Price": 12.95,
-        "Total products": 5
-      }])
-
-    // TODO: Use hook after populating database
-
-    // const hook = () => {
-    //     console.log('effect')
-    //     axios
-    //         .get('http://localhost:3001/Orders')
-    //         .then(response => {
-    //             console.log('promise fulfilled')
-    //             setProducts(response.data.slice(0, 500))
-    //         })
-    // }
-
-    // useEffect(hook, [])
-
-    return (
-      <>
-        <h2>ProductList</h2>
-            {products.map(productInfo => <Product 
-                                            orderId={productInfo["Order ID"]} 
-                                            restaurant={productInfo["Restaurant"]} 
-                                            itemName={productInfo["Item Name"]} 
-                                            quantity={productInfo["Quantity"]} 
-                                            productPrice={productInfo["Product Price"]} 
-                                            totalProducts={productInfo["Total Products"]} 
-                                        />)}
-      </>
-    )
+const ProductList = ({ orders }) => {
+  return (
+    <>
+      <h2>ProductList</h2>
+      {/* Note: Since the data is not going to be updated, I'm using the array index as the key while mapping */}
+      {/* However, if the array were to be updated then I would not do this */}
+      {orders.map((order, i) => <Product key={i} order={order} />)}
+    </>
+  )
 }
   
 export default ProductList;
