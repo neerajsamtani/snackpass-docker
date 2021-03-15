799d43c9aedee7e85e51b1485a2d6393b46bcb9a
Neeraj Samtani <neerajjsamtani@gmail.com>
2021-03-15 05:00:28 -0700

    Connect to Database

--- client/src/App.js
+++ client/src/App.js
@@ -9,22 +9,62 @@ const App = (props) => {
   const [orders, setOrders] = useState([])
 
   const hook = () => {
-      console.log('effect')
       axios
-          .get('/api/orders')
-          .then(response => {
-              console.log('promise fulfilled')
-              setOrders(response.data)
-          })
+      .get('/api/orders')
+      .then(response => {
+          setOrders(response.data)
+      })
   }
 
   useEffect(hook, [])
+
+  // Function copied from https://www.geeksforgeeks.org/get-the-relative-timestamp-difference-between-dates-in-javascript/
+  function timeDiff(curr, prev) { 
+    var ms_Min = 60 * 1000; // milliseconds in Minute 
+    var ms_Hour = ms_Min * 60; // milliseconds in Hour 
+    var ms_Day = ms_Hour * 24; // milliseconds in day 
+    var ms_Mon = ms_Day * 30; // milliseconds in Month 
+    var ms_Yr = ms_Day * 365; // milliseconds in Year 
+    var diff = curr - prev; //difference between dates. 
+    // If the diff is less then milliseconds in a minute 
+    if (diff < ms_Min) { 
+        return Math.round(diff / 1000) + ' seconds ago'; 
+
+        // If the diff is less then milliseconds in a Hour 
+    } else if (diff < ms_Hour) { 
+        return Math.round(diff / ms_Min) + ' minutes ago'; 
+
+        // If the diff is less then milliseconds in a day 
+    } else if (diff < ms_Day) { 
+        return Math.round(diff / ms_Hour) + ' hours ago'; 
+
+        // If the diff is less then milliseconds in a Month 
+    } else if (diff < ms_Mon) { 
+        return 'Around ' + Math.round(diff / ms_Day) + ' days ago'; 
+
+        // If the diff is less then milliseconds in a year 
+    } else if (diff < ms_Yr) { 
+        return 'Around ' + Math.round(diff / ms_Mon) + ' months ago'; 
+    } else { 
+        return 'Around ' + Math.round(diff / ms_Yr) + ' years ago'; 
+    } 
+} 
+
+const currentTime = new Date("2021-03-14 12:17:00")
 
   return (
     <div className="App">
         <Header />
         <ProductList />
-        {orders}
+        {orders.map(order => (
+          <div>
+            <p> <strong>Item:</strong> {order.itemName} </p>
+            <p> <strong>Price:</strong> {order.price} </p>
+            <p> <strong>Times Ordered:</strong> {order.totalOrders} </p>
+            <p> <strong>Last Ordered:</strong> {timeDiff(currentTime, new Date(order.mostRecentOrder))} </p>
+            <br/>
+          </div>
+          ))}
     </div>
   )
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,21 +1,21 @@
 // server/src/server.js
 const express = require("express");
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 // Assign environment variables
 const port = process.env.PORT || 4000;
-const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/test";
-
-/**
- * Setup services
- */
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/db";
 
 // Initiliase an express server
 const app = express();
 
 // Options to pass to mongodb to avoid deprecation warnings
 const options = {
-  useNewUrlParser: true
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  useFindAndModify: false, 
+  useCreateIndex: true
 };
 
 // Function to connect to the database
@@ -37,32 +37,49 @@ db.on("error", err => {
 });
 db.once("open", () => console.log("Successfully connected to mongo"));
 
-// Setup routes to respond to client
-app.get("/api/orders", async (req, res) => {
-  console.log("Client request received");
-  const user = await User.find().exec();
-  console.log(user[0].name);
-  res.send(
-    [`Hello Client! There is one record in the database for ${user[0].name}`,
-    " | Hard Coded String"]
-  );
+const orderSchema = new mongoose.Schema({
+  orderID: Number,
+  restaurant: String,
+  itemName: String,
+  quantity: Number,
+  productPrice: Number,
+  totalProducts: Number,
+  orderTime: Date
+})
+const Order = mongoose.model("Order", orderSchema);
+const order = new Order({
+  orderID: 25583,
+  restaurant: "Hogwarts",
+  itemName: "Minty Sauce",
+  quantity: 1,
+  productPrice: 0.5,
+  totalProducts: 12
 });
 
-// Setup a record in the database to retrieve
-const { Schema } = mongoose;
-const userSchema = new Schema(
-  {
-    name: String
-  },
-  {
-    timestamps: true
-  }
-);
-const User = mongoose.model("User", userSchema);
-const user = new User({ name: "Big Bill Brown" });
-user
-  .save()
-  .then(user => console.log(`${user.name} saved to the database`))
-  .catch(err => console.log(err));
+// Setup routes to respond to client
+// app.get("/api/orders", async (req, res) => {
+//   console.log("Client request received");
+//   const order = await Order.find().exec();
+//   console.log(order);
+//   res.send(
+//     [`Hello Client! There is one record in the database for ${order[0]["Item Name"]}`,
+//     " | Hard Coded String"]
+//   );
+// });
+
+app.get("/api/orders", async (req, res) => {
+  console.log("Client request received");
+  // const order = await Order.find({'Item Name': 'Garlic Naan'}).exec();
+  const agg = await Order.aggregate([
+      {$group: {_id : {itemName : '$Item Name', price:'$Product Price'}, totalOrders:{$sum :1}, mostRecentOrder:{$max :'$Order Time'}}},
+      {$project : {itemName : '$_id.itemName', price : '$_id.price', totalOrders : '$totalOrders', mostRecentOrder: '$mostRecentOrder', _id : 0}},
+      { $sort : { mostRecentOrder : -1 } }
+      // ,      { $limit : 25 }
+  ])
+  await res.json(agg)
+});
+
+// Setup a records in the database to retrieve
+// mongoimport --type csv -d db -c orders --headerline --drop ordersWithTime.csv
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
