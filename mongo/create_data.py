a17db252c5ece9dfcc6922b746e90c94cb1c2c3f
Neeraj Samtani <neerajjsamtani@gmail.com>
2021-03-15 18:15:01 -0700

    Generate Time in ISODate Format

--- mongo/create_data.py
+++ mongo/create_data.py
@@ -4,17 +4,18 @@ import datetime
 import csv
 
 def generate_random_time():
-    random_day = 13 - random.randint(0, 3)
+    random_day = 13 - random.randint(0, 2)
     # Generate peaks around noon and 9pm, relating to lunch and dinner
     if random.random() < 0.5:
         date = datetime.datetime(2021, 3, random_day, 12, 0)
     else:
         date = datetime.datetime(2021, 3, random_day, 21, 0)
     
-    time_offset = np.random.normal(loc=0, scale=240)
+    time_offset = np.random.normal(loc=0, scale=180)
     final_date = date + datetime.timedelta(minutes=time_offset)
 
-    return final_date.strftime("%Y-%m-%d %H:%M:%S")
+    # Store time in ISO Date Format
+    return final_date.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 orderTimeDict = {}
 
@@ -22,6 +23,10 @@ readCSVFile = open("orders.csv", newline='')
 writeCSVFile = open("ordersWithTime.csv", 'w+', newline='')
 reader = csv.reader(readCSVFile, delimiter=',')
 writer = csv.writer(writeCSVFile, delimiter=',')
+
+row1 = next(reader)
+row1.append("Order Time")
+writer.writerow(row1)
 
 for row in reader:
     orderId = row[0]
